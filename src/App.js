c7cb519b514b51cf7011e37dbd61b7cddc5edbde
Lesly <lnombertoc@gmail.com>
2022-01-04 16:04:03 -0500

    quitando estilos

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App max-w-screen-2xl m-auto grid grid-flow-row-dense grid-cols-3 grid-rows-3'>
-      <sidebar className='bg-orange-900 row-span-2'>sidebar</sidebar>
-      <main className='bg-purple-500 col-span-2'>main</main>
-      <footer className='bg-red-200 col-span-2'>footer</footer>
+    <div className='App'>
+      <sidebar className=''>sidebar</sidebar>
+      <main className=''>main</main>
+      <footer className=''>footer</footer>
     </div>
   )
 }
