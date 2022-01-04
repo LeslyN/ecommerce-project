760eb9422c77b21a67c2f8fef0bd0209b91a2833
Lesly <lnombertoc@gmail.com>
2022-01-03 19:58:58 -0500

    pruebas de estilos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-orange-700">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link bg-violet-800"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
